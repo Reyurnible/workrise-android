3aee044ceed9f864c77d30286a36192203e7e879
Shun Hosaka <reyurnible@gmail.com>
2017-09-05 00:32:30 +0900

    [fix] percerable

--- app/src/main/java/io/reyurnible/android/workrise/domain/model/value/YearMonthDay.kt
+++ app/src/main/java/io/reyurnible/android/workrise/domain/model/value/YearMonthDay.kt
@@ -2,32 +2,24 @@ package io.reyurnible.android.workrise.domain.model.value
 
 import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.text.SimpleDateFormat
 import java.util.*
 
 /**
  * YearMonthDay(yyyy-MM-dd) format date value object.
  */
+@Parcelize
 data class YearMonthDay(
         val year: Int,
         val month: Int,
         val day: Int
 ) : Parcelable {
-    companion object CREATOR : Parcelable.Creator<YearMonthDay> {
+    companion object {
         private val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd")
 
         fun createFromString(formattedText: String): YearMonthDay = YearMonthDay(simpleDateFormat.parse(formattedText))
-
-        override fun createFromParcel(parcel: Parcel): YearMonthDay = YearMonthDay(parcel)
-
-        override fun newArray(size: Int): Array<YearMonthDay?> = arrayOfNulls(size)
     }
-
-    constructor(parcel: Parcel) : this(
-            year = parcel.readInt(),
-            month = parcel.readInt(),
-            day = parcel.readInt()
-    )
 
     constructor(calendar: Calendar) : this(
             year = calendar.get(Calendar.YEAR),
@@ -38,12 +30,4 @@ data class YearMonthDay(
     constructor(date: Date) : this(calendar = Calendar.getInstance().apply { time = date })
 
     override fun toString(): String = String.format("%04d-%02d-%02d", year, month, day)
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(year)
-        parcel.writeInt(month)
-        parcel.writeInt(day)
-    }
-
-    override fun describeContents(): Int = 0
 }
