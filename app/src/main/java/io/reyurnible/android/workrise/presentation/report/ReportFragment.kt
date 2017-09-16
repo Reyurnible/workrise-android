de4e6bb57f547cb29c1e1bf11f3d7243fba5e971
Shun Hosaka <reyurnible@gmail.com>
2017-09-16 16:09:31 +0900

    [feature] impl report scene

--- app/src/main/java/io/reyurnible/android/workrise/presentation/report/ReportFragment.kt
+++ app/src/main/java/io/reyurnible/android/workrise/presentation/report/ReportFragment.kt
@@ -6,19 +6,33 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import dagger.android.support.AndroidSupportInjection
 import io.reyurnible.android.workrise.R
+import io.reyurnible.android.workrise.domain.model.entity.Form
+import io.reyurnible.android.workrise.domain.model.entity.Report
 import io.reyurnible.android.workrise.domain.model.value.YearMonthDay
+import io.reyurnible.android.workrise.infrastructure.view.ChecklistFormView
+import io.reyurnible.android.workrise.infrastructure.view.TextFormView
+import io.reyurnible.android.workrise.presentation.common.invisible
+import io.reyurnible.android.workrise.presentation.common.visible
 import io.reyurnible.android.workrise.presentation.form.FormActivity
 import io.reyurnible.android.workrise.presentation.form.createIntent
 import kotlinx.android.synthetic.main.report_fragment.*
+import javax.inject.Inject
 import kotlin.properties.Delegates
 
 class ReportFragment : Fragment(), ReportPresenter.ReportView {
-
     companion object;
 
     private val date: YearMonthDay by bindDate()
+    override var report: Report? = null
+        set(value) {
+            field = value
+            inflateReport(value)
+        }
+
+    @Inject lateinit var presenter: ReportPresenter
 
     override fun onAttach(context: Context?) {
         AndroidSupportInjection.inject(this)
@@ -30,8 +44,38 @@ class ReportFragment : Fragment(), ReportPresenter.ReportView {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        createButton.setOnClickListener {
-            startActivity(FormActivity.createIntent(activity, date))
+        presenter.initialize(this, date)
+    }
+
+    override fun onDestroyView() {
+        presenter.release()
+        super.onDestroyView()
+    }
+
+    override fun showErrorDialog(error: Throwable) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    /**
+     * Report表示用のViewの作成
+     */
+    private fun inflateReport(report: Report?) {
+        report?.run {
+            createButton.invisible()
+            val layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            content.map { form ->
+                when (form) {
+                    is Form.CheckList -> ChecklistFormView(context).also { view -> view.form = form }
+                    is Form.Text -> TextFormView(context).also { view -> view.form = form }
+                }
+            }.forEach { view ->
+                reportContainerLayout.addView(view, layoutParams)
+            }
+        } ?: let {
+            createButton.visible()
+            createButton.setOnClickListener {
+                startActivity(FormActivity.createIntent(activity, date))
+            }
         }
     }
 
--- app/src/main/java/io/reyurnible/android/workrise/presentation/report/ReportPresenter.kt
+++ app/src/main/java/io/reyurnible/android/workrise/presentation/report/ReportPresenter.kt
@@ -1,14 +1,33 @@
 package io.reyurnible.android.workrise.presentation.report
 
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reyurnible.android.workrise.common.addDisposableToBag
+import io.reyurnible.android.workrise.domain.model.entity.Report
+import io.reyurnible.android.workrise.domain.model.identifier.ReportId
+import io.reyurnible.android.workrise.domain.model.value.YearMonthDay
+import io.reyurnible.android.workrise.usecase.GetReportUseCase
+import javax.inject.Inject
+import kotlin.properties.Delegates
 
-class ReportPresenter {
-
+class ReportPresenter
+@Inject constructor(
+        private val getReportUseCase: GetReportUseCase
+) {
+    private var date: YearMonthDay by Delegates.notNull()
     private lateinit var view: ReportPresenter.ReportView
     private val disposableBag: CompositeDisposable = CompositeDisposable()
 
-    fun initialize(view: ReportPresenter.ReportView) {
+    fun initialize(view: ReportPresenter.ReportView, date: YearMonthDay) {
+        this.date = date
         this.view = view
+        getReportUseCase.get(id = ReportId(date))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ report ->
+                    view.report = report
+                }, { error ->
+                    view.showErrorDialog(error)
+                }).addDisposableToBag(disposableBag)
     }
 
     fun release() {
@@ -16,7 +35,8 @@ class ReportPresenter {
     }
 
     interface ReportView {
-
+        var report: Report?
+        fun showErrorDialog(error: Throwable)
     }
 
 }
