00ab3786c3869b4f636c5c36c5fc3bf0e7de4790
Shun Hosaka <reyurnible@gmail.com>
2017-10-21 23:45:29 +0900

    [fix] rename report setting factory

--- app/src/main/java/io/reyurnible/android/workrise/domain/repository/ReportSettingRepositoryImpl.kt
+++ app/src/main/java/io/reyurnible/android/workrise/domain/repository/ReportSettingRepositoryImpl.kt
@@ -4,15 +4,15 @@ import android.content.Context
 import io.reactivex.Completable
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
+import io.reyurnible.android.workrise.domain.factory.defaultInstance
 import io.reyurnible.android.workrise.domain.model.entity.ReportSetting
-import io.reyurnible.android.workrise.domain.service.ReportSettingProvider
 import io.reyurnible.android.workrise.infrastructure.pref.WorkrisePreferences
 
 class ReportSettingRepositoryImpl(private val context: Context, private val workrisePreferences: WorkrisePreferences) : ReportSettingRepository {
 
     override fun fetchReportSetting(): Single<ReportSetting> =
             Single.create<ReportSetting> { source ->
-                (workrisePreferences.reportSetting ?: ReportSettingProvider.createDefaultReportSetting(context))
+                (workrisePreferences.reportSetting ?: ReportSetting.Factory.defaultInstance(context))
                         .let(source::onSuccess)
             }.subscribeOn(Schedulers.io())
 
